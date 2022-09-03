0d396cd4f81198b52ca481c4e13fc647fdba16ba
gulu2016 <2412165411@qq.com>
2022-09-03 11:27:05 +0800

    page1_x

---
+++ Page1_4_Helloworld/Program.cs
@@ -0,0 +1,28 @@
+﻿// C# program to print Hello World!
+
+//p1-4-s1-4 System is a namespace which contains the commonly used types.
+// It is specified with a using System directive.这里相当于引用其他namespace的内容
+using System;
+
+//p1-4-s1-3 namespace declaration
+namespace HelloWorldApp
+{
+
+    // Class declaration
+    class Geeks
+    {
+
+        // Main Method
+        static void Main(string[] args)
+        {
+
+            // statement
+            //p1-4-s1-1 printing Hello World!
+            Console.WriteLine("Hello World!");
+
+            //p1-4-s1-2 To prevents the screen from
+            // running and closing quickly
+            Console.ReadKey();
+        }
+    }
+}
