ed0a76b63b504bf2c9bb5aed6fd2598e133f5dea
gulu2016 <2412165411@qq.com>
2022-09-03 11:15:23 +0800

    page3_x

---
+++ Page3_4_JumeStatements/Program.cs
@@ -0,0 +1,39 @@
+﻿// C# Program to illustrate the use
+// of throw keyword
+using System;
+
+class Geeks
+{
+
+    // taking null in the string
+    static string sub = null;
+
+    // method to display subject name
+    static void displaysubject(string sub1)
+    {
+        if (sub1 == null)
+            //p3_4-s1-1 使用throw抛出异常
+            throw new NullReferenceException("Exception Message dddd");
+
+    }
+
+    // Main Method
+    static void Main(string[] args)
+    {
+
+        // using try catch block to
+        // handle the Exception
+        try
+        {
+
+            // calling the static method
+            displaysubject(sub);
+        }
+
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp.Message);
+        }
+    }
+
+}
