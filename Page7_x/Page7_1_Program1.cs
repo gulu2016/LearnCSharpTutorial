20d85a531f2cd6557c5861c3ed39778dfcba4595
gulu2016 <2412165411@qq.com>
2022-09-11 23:08:35 +0800

    page7_1

---
+++ Page7_x/Page7_1_Program1.cs
@@ -0,0 +1,50 @@
+﻿// C# program to demonstrate the ArrayList
+using System;
+using System.Collections;
+
+class GFG_p7_1_program1
+{
+
+    // Main Method
+    static public void Main_p7_1_program1()
+    {
+
+        // Creating ArrayList
+        //p7_1-s1-1 创建一个ArrayList
+        ArrayList My_array = new ArrayList();
+
+        // Adding elements in the
+        // My_array ArrayList
+        // This ArrayList contains elements
+        // of different types
+        //p7_1-s1-2 向ArrayList中添加元素
+        My_array.Add(12.56);
+        My_array.Add("GeeksforGeeks");
+        My_array.Add(null);
+        My_array.Add('G');
+        My_array.Add(1234);
+        My_array.Add(12.56);
+        My_array.Add("GeeksforGeeks");
+        My_array.Add(null);
+        My_array.Add('G');
+        My_array.Add(1234);
+
+        // Accessing the elements
+        // of My_array ArrayList
+        // Using foreach loop
+        //p7_1-s1-3 遍历ArrayList的元素
+        foreach (var elements in My_array)
+        {
+            Console.WriteLine(elements);
+        }
+
+
+        // Displaying count of elements of ArrayList
+        //p7_1-s1-4 Count返回ArrayList中的元素个数
+        Console.WriteLine("Number of elements: " + My_array.Count);
+
+        // Displaying Current capacity of ArrayList
+        //p7_1-s1-5 Capacity返回ArrayList中的容量大小
+        Console.WriteLine("Current capacity: " + My_array.Capacity);
+    }
+}
---
+++ Page7_x/Page7_1_Program2.cs
@@ -0,0 +1,47 @@
+﻿// C# program to illustrate
+// sorting of ArrayList
+using System;
+using System.Collections;
+
+public class GFG
+{
+    static public void Main()
+    {
+
+        // Creating ArrayList
+        ArrayList My_array = new ArrayList();
+
+        // Adding elements in the
+        // My_array ArrayList
+        // This ArrayList contains
+        // elements of the same types
+        My_array.Add(1);
+        My_array.Add(6);
+        My_array.Add(40);
+        My_array.Add(10);
+        My_array.Add(5);
+        My_array.Add(3);
+        My_array.Add(2);
+        My_array.Add(4);
+
+        // ArrayList before sorting
+        Console.WriteLine("ArrayList before using Sort() method:");
+
+        foreach (var elements in My_array)
+        {
+            Console.WriteLine(elements);
+        }
+
+        // Sort the elements of the ArrayList
+        // Using sort() method
+        //p7_1-s2-1 对ArrayList中的元素进行排序
+        My_array.Sort();
+
+        // ArrayList after sorting
+        Console.WriteLine("ArrayList after using Sort() method:");
+        foreach (var elements in My_array)
+        {
+            Console.WriteLine(elements);
+        }
+    }
+}
