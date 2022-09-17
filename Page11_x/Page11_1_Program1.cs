d12a2b0ea93fccbdd613ac111ff7bf05124687e1
gulu2016 <2412165411@qq.com>
2022-09-17 12:14:35 +0800

    page11

---
+++ Page11_x/Page11_1_Program1.cs
@@ -0,0 +1,47 @@
+﻿// C# program to illustrate the
+// read and write property
+using System;
+
+public class Student
+{
+
+    // Declare name field
+    private string name = "GeeksforGeeks";
+
+    // Declare name property
+    //p11_1-s1-1 带有get和 set的property
+    public string Name
+    {
+
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+}
+
+class TestStudent
+{
+
+    // Main Method
+    public static void Main(string[] args)
+    {
+        Student s = new Student();
+
+        // calls set accessor of the property Name,
+        // and pass "GFG" as value of the
+        // standard field 'value'.
+        //p11_1-s1-2 使用set方法赋值
+        s.Name = "GFG";
+
+        // displays GFG, Calls the get accessor
+        // of the property Name.
+        //p11_1-s1-3 使用get方法读取属性值
+        Console.WriteLine("Name: " + s.Name);
+    }
+}
